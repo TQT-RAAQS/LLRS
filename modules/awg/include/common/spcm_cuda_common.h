1f6f713c6f30487e8887b61b6ef955868fb7e3b2
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-03-08 10:06:08 -0500

    Added AWG module

---
+++ modules/awg/include/common/spcm_cuda_common.h
@@ -0,0 +1,16 @@
+// ----- This file contains functions that are used by multiple CUDA RDMA examples -----
+
+#include "../c_header/dlltyp.h"
+#include <cufft.h>
+
+// ----- Init CUDA device without RDMA support (e.g. on Windows) ----
+bool bInitCUDADevice (int lCUDADevIdx);
+
+// ----- This function creates a DMA buffer on the GPU and sets it up for use as RDMA buffer -----
+// -- lCudaDevIdx:              in      Index of CUDA device in the system, starting with 0
+// -- qwDMABufferSize_bytes:    in      Size of DMA buffer that should be created on GPU
+// -- returns address of created DMA buffer on GPU
+void* pvGetRDMABuffer (int lCUDADevIdx, size_t qwDMABufferSize_bytes);
+
+// ---returns description for CUDA FFT error
+const char* szCudaGetErrorText (cufftResult eError);
---
+++ modules/awg/include/spcm_includes.h
@@ -0,0 +1,13 @@
+#ifndef _SPCMINCLUDES_H_
+#define _SPCMINCLUDES_H_
+
+// ----- include standard driver header from library -----
+#include "c_header/dlltyp.h"
+#include "c_header/regs.h"
+#include "c_header/spcerr.h"
+#include "c_header/spcm_drv.h"
+#include "common/ostools/spcm_oswrap.h"
+#include "common/ostools/spcm_ostools.h"
+// #include "common/spcm_cuda_common.h"
+
+#endif
