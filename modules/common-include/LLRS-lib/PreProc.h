d551acb3263b33439be26653c509956c3d12f91e
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-03-12 10:35:34 -0400

    Fixed compile error for the paths in preproc

--- modules/common-include/LLRS-lib/PreProc.h
+++ modules/common-include/LLRS-lib/PreProc.h
@@ -9,6 +9,8 @@
 #include <cstdlib>
 #include <experimental/filesystem>
 #include <sstream>
+#include <iostream>
+#include <iomanip>
 
 // Define separator based on the platform
 #ifndef _K_PATH_SEPARATOR
@@ -23,18 +25,18 @@ const char kPathSeparator1 = '/';
 namespace fs = std::experimental::filesystem;
 
 #define FILE_EXISTS(name) (fs::exists(name))
+std::string NADA = "";
 
+#define PSF_PATH(fname) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "psf" + kPathSeparator1 + (fname))
+#define COEF_X_PATH(fname) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "coef" + kPathSeparator1 + "primary" + kPathSeparator1 + (fname))
+#define COEF_Y_PATH(fname) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "coef" + kPathSeparator1 + "secondary" + kPathSeparator1 + (fname))
+#define WF_REPO_PATH(fname) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "wfm" + kPathSeparator1 + (fname))
+#define LOGGING_PATH(fname) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "logs" + kPathSeparator1 + (fname))
 
-#define PSF_PATH(fname) (PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "psf" + kPathSeparator1 + (fname))
-#define COEF_X_PATH(fname) (PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "coef" + kPathSeparator1 + "primary" + kPathSeparator1 + (fname))
-#define COEF_Y_PATH(fname) (PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "coef" + kPathSeparator1 + "secondary" + kPathSeparator1 + (fname))
-#define WF_REPO_PATH(fname) (PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "wfm" + kPathSeparator1 + (fname))
-#define LOGGING_PATH(fname) (PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "logs" + kPathSeparator1 + (fname))
-
-#define CONFIG_PATH(id) (PROJECT_BASE_DIR + kPathSeparator1 + "configs" + kPathSeparator1 + "LLRS" + kPathSeparator1 + (id))
-#define SOLN_PATH(id) (PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "operational_solutions" + kPathSeparator1 + (id) + ".json")
-#define BENCHMARK_PATH(id) (PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "runtime_benchmarks" + kPathSeparator1 + (id) + ".json")
-#define IMAGE_PATH(epoch) (PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "images" + kPathSeparator1 + (epoch) + ".pgm")
+#define CONFIG_PATH(id) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "configs" + kPathSeparator1 + "LLRS" + kPathSeparator1 + (id))
+#define SOLN_PATH(id) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "operational_solutions" + kPathSeparator1 + (id) + ".json")
+#define BENCHMARK_PATH(id) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "runtime_benchmarks" + kPathSeparator1 + (id) + ".json")
+#define IMAGE_PATH(epoch) (NADA + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "images" + kPathSeparator1 + (epoch) + ".pgm")
 
 #define TRIAL_NAME(num) ("trial_" + std::to_string(num))
 #define REP_NAME(num) ("repetition_" + std::to_string(num))
