e85afa5e931e6bc0016865739a1f5427c2aeae2d
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-03-13 10:45:05 -0400

    Added the main component of the llcs module

---
+++ modules/llcs/main.cpp
@@ -0,0 +1,35 @@
+/**
+ * @brief   Entry point for the Low Latency FSM. 
+ * @date    Oct 2023
+ */
+
+#include "llcs/common.hpp"
+#include "fsm.hpp"
+#include "server.hpp"
+#include "awg.hpp"
+#include "trigger-detector.hpp"
+
+
+std::atomic<bool> g_interrupted(false);
+
+void SignalHandler(int signum) {
+    std::cout << "Interrupt signal (" << signum << ") received.\n";
+    g_interrupted.store(true);
+}
+
+int main( int argc, char *argv[] ){
+    
+    Server *server = new Server();
+    TriggerDetector<AWG> *td = new TriggerDetector<AWG>();
+    FiniteStateMachine<AWG> *fsm = new FiniteStateMachine<AWG>(server, td);
+
+    fsm->runFSM();
+
+    delete td;
+    delete server;
+    delete fsm;
+
+    std::cout << "Program terminated gracefully" << std::endl;
+
+    return SYS_OK;    
+} 
