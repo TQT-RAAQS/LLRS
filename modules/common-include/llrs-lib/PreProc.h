b3f2d81d33f15e0dc5bbfa392685bbd97874e7b4
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-03-19 11:42:11 -0400

    Changed the path for logging

--- modules/common-include/llrs-lib/PreProc.h
+++ modules/common-include/llrs-lib/PreProc.h
@@ -31,7 +31,7 @@ namespace fs = std::experimental::filesystem;
 #define COEF_X_PATH(fname) (std::string("") + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "coef" + kPathSeparator1 + "primary" + kPathSeparator1 + (fname))
 #define COEF_Y_PATH(fname) (std::string("") + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "coef" + kPathSeparator1 + "secondary" + kPathSeparator1 + (fname))
 #define WF_REPO_PATH(fname) (std::string("") + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "wfm" + kPathSeparator1 + (fname))
-#define LOGGING_PATH(fname) (std::string("") + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "logs" + kPathSeparator1 + (fname))
+#define LOGGING_PATH(fname) (std::string("") + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "runtime" + kPathSeparator1 + "logs" + kPathSeparator1 + (fname))
 
 #define CONFIG_PATH(id) (std::string("") + PROJECT_BASE_DIR + kPathSeparator1 + "configs" + kPathSeparator1 + "LLRS" + kPathSeparator1 + (id))
 #define SOLN_PATH(id) (std::string("") + PROJECT_BASE_DIR + kPathSeparator1 + "resources" + kPathSeparator1 + "operational_solutions" + kPathSeparator1 + (id) + ".json")
