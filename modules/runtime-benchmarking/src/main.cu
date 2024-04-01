654ca5f21fa7983392e3a0f5d17b30a0abebdc49
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-04-01 10:58:43 -0400

    Formatted run-bench files

--- modules/runtime-benchmarking/src/main.cu
+++ modules/runtime-benchmarking/src/main.cu
@@ -1,26 +1,29 @@
-#include "llrs.h"
 #include "awg.hpp"
-#include <fstream>
+#include "llrs.h"
 #include <cstdlib>
+#include <fstream>
 #include <memory>
 
-int main(int argc, char * argv[]){
-    
+int main(int argc, char *argv[]) {
+
     // Read problem statement
     std::string problem_id;
     std::string problem_config;
     if (argc > 2) {
-        problem_config = std::string(argv[1]); 
-        problem_id = 	 std::string(argv[2]); 
+        problem_config = std::string(argv[1]);
+        problem_id = std::string(argv[2]);
     } else {
-		std::cout << "For runtime benchmarking, please provide problem_id and problem_config." << std::endl; 
+        std::cout << "For runtime benchmarking, please provide problem_id and "
+                     "problem_config."
+                  << std::endl;
         return LLRS_ERR;
     }
 
-    std::shared_ptr<AWG> awg {std::make_shared<AWG>()};
-    LLRS<AWG> l {awg}; 
+    std::shared_ptr<AWG> awg{std::make_shared<AWG>()};
+    LLRS<AWG> l{awg};
     int16 *pnData = nullptr;
-    int qwBufferSize = awg->allocate_transfer_buffer(awg->get_samples_per_segment(), pnData);
+    int qwBufferSize =
+        awg->allocate_transfer_buffer(awg->get_samples_per_segment(), pnData);
     awg->fill_transfer_buffer(pnData, awg->get_samples_per_segment(), 0);
     awg->init_and_load_all(pnData, awg->get_samples_per_segment());
     vFreeMemPageAligned(pnData, qwBufferSize);
