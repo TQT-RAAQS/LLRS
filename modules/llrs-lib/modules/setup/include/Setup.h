5a05309fd63af37fdeed7d2054de438f00c908a5
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-04-02 13:45:28 -0400

    Fixed typo

--- modules/llrs-lib/modules/setup/include/Setup.h
+++ modules/llrs-lib/modules/setup/include/Setup.h
@@ -18,7 +18,7 @@ int read_fparams(std::string file_name, std::vector<Synthesis::WP> &params,
                  const int limit);
 int create_wf_repo(size_t Nt_x, size_t Nt_y, double sample_rate,
                    double wf_duration, int wavefrom_length, int waveform_mask,
-                   int vpp, , std::string coef_x_fname,
+                   int vpp, std::string coef_x_fname,
                    std::string coef_y_fname);
 Synthesis::WaveformTable create_wf_table(size_t Nt_x, size_t Nt_y,
                                          double sample_rate, double wf_duration,
