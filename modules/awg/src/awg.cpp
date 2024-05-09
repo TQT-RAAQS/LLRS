7ac97f198a06e9cc29fb322914c4ac957ad545e1
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-05-09 15:38:09 -0400

    Changes to the AWG modules

--- modules/awg/src/awg.cpp
+++ modules/awg/src/awg.cpp
@@ -24,7 +24,7 @@ AWG::~AWG() {
  * @param filename name of file
  * @return status code
  */
-uint32 AWG::read_config(std::string filename) {
+int AWG::read_config(std::string filename) {
     YAML::Node node;
 
     /// Open file
@@ -33,7 +33,7 @@ uint32 AWG::read_config(std::string filename) {
     } catch (const YAML::BadFile &e) {
         std::cerr << "Error loading YAML file (awg.cpp)." << std::endl;
         std::cerr << "ERROR: " << e.what() << std::endl;
-        return AWG_ERR;
+        return 1;
     }
 
     /// Extract file contents
@@ -60,75 +60,62 @@ uint32 AWG::read_config(std::string filename) {
     config.acq_timeout = node["acq_timeout"].as<int>();
     config.async_trig_amp = node["async_trig_amp"].as<int>();
 
-    return AWG_OK;
+    return 0;
 }
 
 /**
  * @brief Configure the AWG
  * @return Error code
  */
-uint32 AWG::configure() {
+int AWG::configure() {
     if (read_config(AWG_CONFIG_PATH) != AWG_OK) {
         std::cerr << "Error occured in parsing AWG config.\n";
+        return 1;
     }
 
     p_card = spcm_hOpen(config.driver_path);
     if (!p_card) {
         std::cerr << "ERROR: AWG Card not found.\n";
+        return 1;
     } else {
         reset_card();
     }
+    status = 0;
+    status |= enable_channels(config.channels);
+    status |= enable_outputs(config.channels, config.amp);
+    status |= set_sample_rate(config.sample_rate);
+    status |= set_external_clock_mode(config.external_clock_freq);
+    status |= set_trigger_settings();
+    status |= set_dout_async(SPCM_X0_MODE); /// x0 - null counter
+    status |=
+        set_dout_trigger_mode(SPCM_X1_MODE,
+                              SPCM_XMODE_DIGOUTSRC_CH1); /// x1 - take image
+    status |= set_dout_async(SPCM_X2_MODE); /// x2 - done/resume clock
 
-    enable_channels(config.channels);
-    enable_outputs(config.channels, config.amp);
-    set_sample_rate(config.sample_rate);
-    set_external_clock_mode(config.external_clock_freq);
-    set_trigger_settings();
-    set_dout_async(SPCM_X0_MODE); /// x0 - null counter
-    set_dout_trigger_mode(SPCM_X1_MODE,
-                          SPCM_XMODE_DIGOUTSRC_CH1); /// x1 - take image
-    set_dout_async(SPCM_X2_MODE);                    /// x2 - done/resume clock
-
-    uint32 _max_step;
-    int _max_temp_step;
-    int _bps;
-    int _num_channels;
-    uint32 _max_seg = static_cast<uint32>(config.awg_num_segments);
-
-    spcm_dwGetParam_i32(p_card, SPC_SEQMODE_AVAILMAXSTEPS, &_max_temp_step);
-    spcm_dwGetParam_i32(p_card, SPC_MIINST_BYTESPERSAMPLE, &_bps);
-    spcm_dwGetParam_i32(p_card, SPC_CHCOUNT, &_num_channels);
-    _max_step = static_cast<uint32>(_max_temp_step);
-    if (_max_seg == 0 || ((_max_seg & (_max_seg - 1)) != 0) ||
-        _max_seg > _max_step) {
-        std::cerr << "ERROR: StreamSequence Constructor -> max_seg needs to be "
-                     "a power of 2 and less than "
-                  << _max_step << std::endl;
-        return AWG_ERR;
-    }
-
-    max_seg = _max_seg;
-    max_step = _max_step;
-    bps = _bps;
-    lSetChannels = _num_channels;
+    status |= spcm_dwGetParam_i32(p_card, SPC_SEQMODE_AVAILMAXSTEPS, &max_step);
+    status |= spcm_dwGetParam_i32(p_card, SPC_MIINST_BYTESPERSAMPLE, &bps);
+    status |= spcm_dwGetParam_i32(p_card, SPC_CHCOUNT, &lSetChannels);
     dwFactor = 1;
 
-    spcm_dwSetParam_i32(p_card, SPC_CARDMODE, SPC_REP_STD_SEQUENCE);
-    spcm_dwSetParam_i32(p_card, SPC_SEQMODE_MAXSEGMENTS, max_seg);
-    spcm_dwSetParam_i32(p_card, SPC_SEQMODE_STARTSTEP, 0);
+    status |= spcm_dwSetParam_i32(p_card, SPC_CARDMODE, SPC_REP_STD_SEQUENCE);
+    status |= spcm_dwSetParam_i32(p_card, SPC_SEQMODE_MAXSEGMENTS,
+                                  config.awg_num_segments);
+    status |= spcm_dwSetParam_i32(p_card, SPC_SEQMODE_STARTSTEP, 0);
 
-    /// AWG Sequence Mode Global Configuration.
-    const int64_t total_memory_bytes = 4294967296;
-    const int64_t total_sample_capacity = total_memory_bytes / sizeof(short);
-    const int64_t awg_total_segments = config.awg_num_segments;
-    const int64_t samples_per_waveform = config.waveform_length;
-    const int64_t waveforms_per_segment = config.waveforms_per_segment;
-    const int64_t samples_per_segment_max =
-        total_sample_capacity / awg_total_segments;
-    assert(waveforms_per_segment * samples_per_waveform <=
-           samples_per_segment_max);
+    if (config.awg_num_segments == 0 ||
+        ((config.awg_num_segments & (config.awg_num_segments - 1)) != 0) ||
+        config.awg_num_segments > max_step) {
+        std::cerr << "ERROR: AWG Constructor -> max_seg needs to be "
+                     "a power of 2 and less than "
+                  << max_step << std::endl;
+        return 1;
+    }
+    assert(samples_per_segment <=
+           (AWG_MEMORY_SIZE / bps) /
+               config.awg_num_segments); // assert that the samples per segments
+                                         // matches the size of the AWG memory
 
-    return AWG_OK;
+    return status;
 }
 
 /**
@@ -136,9 +123,9 @@ uint32 AWG::configure() {
  * @param channels Vector of channels to be enabled
  * @return Error code
  */
-uint32 AWG::enable_channels(const std::vector<int> &channels) {
+int AWG::enable_channels(const std::vector<int> &channels) {
 
-    uint32 tag = 0;
+    int tag = 0;
     /// update tag with the index of each channel to be turned on
     for (size_t i = 0; i < channels.size(); ++i) {
         switch (channels[i]) {
@@ -161,7 +148,6 @@ uint32 AWG::enable_channels(const std::vector<int> &channels) {
     }
 
     num_channels = channels.size();
-
     return spcm_dwSetParam_i32(p_card, SPC_CHENABLE, tag);
 }
 
@@ -171,34 +157,36 @@ uint32 AWG::enable_channels(const std::vector<int> &channels) {
  * @param amp Vector of amplitudes corresponding to each output channel
  * @return Error code
  */
-uint32 AWG::enable_outputs(const std::vector<int> &channels,
-                           const std::vector<int> &amp) {
+int AWG::enable_outputs(const std::vector<int> &channels,
+                        const std::vector<int> &amp) {
 
+    assert(channels.size() == amp.size());
+    int status = 0;
     for (size_t i = 0; i < channels.size(); ++i) {
         switch (channels[i]) {
         case 0:
-            spcm_dwSetParam_i32(p_card, SPC_AMP0, amp[i]);
-            spcm_dwSetParam_i32(p_card, SPC_ENABLEOUT0, 1);
+            status |= spcm_dwSetParam_i32(p_card, SPC_AMP0, amp[i]);
+            status |= spcm_dwSetParam_i32(p_card, SPC_ENABLEOUT0, 1);
             break;
         case 1:
-            spcm_dwSetParam_i32(p_card, SPC_AMP1, amp[i]);
-            spcm_dwSetParam_i32(p_card, SPC_ENABLEOUT1, 1);
+            status |= spcm_dwSetParam_i32(p_card, SPC_AMP1, amp[i]);
+            status |= spcm_dwSetParam_i32(p_card, SPC_ENABLEOUT1, 1);
             break;
         case 2:
-            spcm_dwSetParam_i32(p_card, SPC_AMP2, amp[i]);
-            spcm_dwSetParam_i32(p_card, SPC_ENABLEOUT2, 1);
+            status |= spcm_dwSetParam_i32(p_card, SPC_AMP2, amp[i]);
+            status |= spcm_dwSetParam_i32(p_card, SPC_ENABLEOUT2, 1);
             break;
         case 3:
-            spcm_dwSetParam_i32(p_card, SPC_AMP3, amp[i]);
-            spcm_dwSetParam_i32(p_card, SPC_ENABLEOUT3, 1);
+            status |= spcm_dwSetParam_i32(p_card, SPC_AMP3, amp[i]);
+            status |= spcm_dwSetParam_i32(p_card, SPC_ENABLEOUT3, 1);
             break;
         default:
             std::cerr << "ERROR: AWG channel not supported\n";
-            break;
+            return 1;
         }
     }
 
-    return AWG_OK;
+    return status;
 }
 
 /**
@@ -206,7 +194,7 @@ uint32 AWG::enable_outputs(const std::vector<int> &channels,
  * @param sample_rate sample rate
  * @return Error code
  */
-uint32 AWG::set_sample_rate(int sample_rate) {
+int AWG::set_sample_rate(int sample_rate) {
     return spcm_dwSetParam_i64(p_card, SPC_SAMPLERATE, sample_rate);
 }
 
@@ -215,11 +203,12 @@ uint32 AWG::set_sample_rate(int sample_rate) {
  * @param external_clock_freq Desired frequency of the external clock
  * @return Error code
  */
-uint32 AWG::set_external_clock_mode(int external_clock_freq) {
-    spcm_dwSetParam_i32(p_card, SPC_CLOCKMODE, SPC_CM_EXTREFCLOCK);
-    spcm_dwSetParam_i32(p_card, SPC_REFERENCECLOCK, external_clock_freq);
-
-    return AWG_OK;
+int AWG::set_external_clock_mode(int external_clock_freq) {
+    int status = 0;
+    status |= spcm_dwSetParam_i32(p_card, SPC_CLOCKMODE, SPC_CM_EXTREFCLOCK);
+    status |=
+        spcm_dwSetParam_i32(p_card, SPC_REFERENCECLOCK, external_clock_freq);
+    return status;
 }
 
 /**
@@ -227,7 +216,7 @@ uint32 AWG::set_external_clock_mode(int external_clock_freq) {
  * @param
  * @return Error code
  */
-uint32 AWG::set_internal_clock_mode() {
+int AWG::set_internal_clock_mode() {
     return spcm_dwSetParam_i32(p_card, SPC_CLOCKMODE, SPC_CM_INTPLL);
 }
 
@@ -235,15 +224,15 @@ uint32 AWG::set_internal_clock_mode() {
  * @brief Sets upper and lower trigger levels and the mode of the trigger
  * @return Error code
  */
-uint32 AWG::set_trigger_settings() {
-    spcm_dwSetParam_i32(p_card, SPC_TRIG_EXT0_LEVEL0, 2000);
-    spcm_dwSetParam_i32(p_card, SPC_TRIG_EXT0_LEVEL1, 500);
-    spcm_dwSetParam_i32(p_card, SPC_TRIG_EXT0_MODE, SPC_TM_NEG | SPC_TM_REARM);
-    spcm_dwSetParam_i32(p_card, SPC_TRIG_ORMASK, SPC_TMASK_EXT0);
-
-    spcm_dwSetParam_i32(p_card, SPC_TIMEOUT, 5000);
-
-    return AWG_OK;
+int AWG::set_trigger_settings() {
+    int status = 0;
+    status |= spcm_dwSetParam_i32(p_card, SPC_TRIG_EXT0_LEVEL0, 2000);
+    status |= spcm_dwSetParam_i32(p_card, SPC_TRIG_EXT0_LEVEL1, 500);
+    status |= spcm_dwSetParam_i32(p_card, SPC_TRIG_EXT0_MODE,
+                                  SPC_TM_NEG | SPC_TM_REARM);
+    status |= spcm_dwSetParam_i32(p_card, SPC_TRIG_ORMASK, SPC_TMASK_EXT0);
+    status |= spcm_dwSetParam_i32(p_card, SPC_TIMEOUT, 5000);
+    return status;
 }
 
 /**
@@ -251,10 +240,10 @@ uint32 AWG::set_trigger_settings() {
  * logical 1 is set for each sample as the most significant bit.
  * @return Error code
  */
-uint32 AWG::set_dout_trigger_mode(int32 line, int32 channel) {
+int AWG::set_dout_trigger_mode(int32 line, int32 channel) {
     int trigger_mode =
         (SPCM_XMODE_DIGOUT | channel | SPCM_XMODE_DIGOUTSRC_BIT15);
-    return spcm_dwSetParam_i32(this->p_card, line, trigger_mode);
+    return spcm_dwSetParam_i32(p_card, line, trigger_mode);
 }
 
 /**
@@ -262,8 +251,8 @@ uint32 AWG::set_dout_trigger_mode(int32 line, int32 channel) {
  * needs to be called after a change in setting.
  * @return Error code
  */
-uint32 AWG::set_dout_async(int32 line) {
-    return spcm_dwSetParam_i32(this->p_card, line, SPCM_XMODE_ASYNCOUT);
+int AWG::set_dout_async(int32 line) {
+    return spcm_dwSetParam_i32(p_card, line, SPCM_XMODE_ASYNCOUT);
 }
 
 /**
@@ -271,23 +260,21 @@ uint32 AWG::set_dout_async(int32 line) {
  * SPMCM_XMODE_ASYNCOUT
  * @return Error code
  */
-uint32 AWG::generate_async_output_pulse(TriggerType type) {
+void AWG::generate_async_output_pulse(TriggerType type) {
     switch (type) {
     case EMCCD:
-        spcm_dwSetParam_i32(this->p_card, SPCM_XX_ASYNCIO, 0);
-        spcm_dwSetParam_i32(this->p_card, SPCM_XX_ASYNCIO,
-                            config.async_trig_amp);
-        spcm_dwSetParam_i32(this->p_card, SPCM_XX_ASYNCIO, 0);
+        spcm_dwSetParam_i32(p_card, SPCM_XX_ASYNCIO, 0);
+        spcm_dwSetParam_i32(p_card, SPCM_XX_ASYNCIO, config.async_trig_amp);
+        spcm_dwSetParam_i32(p_card, SPCM_XX_ASYNCIO, 0);
     }
-    return AWG_OK;
 }
 
 /**
  * @brief Software trigger on card start
  * @return Error code
  */
-uint32 AWG::start_stream() {
-    return spcm_dwSetParam_i32(this->p_card, SPC_M2CMD,
+int AWG::start_stream() {
+    return spcm_dwSetParam_i32(p_card, SPC_M2CMD,
                                M2CMD_CARD_START | M2CMD_CARD_FORCETRIGGER);
 }
 
@@ -295,7 +282,7 @@ uint32 AWG::start_stream() {
  * @brief Reset the card
  * @return Error code
  */
-uint32 AWG::reset_card() {
+int AWG::reset_card() {
     return spcm_dwSetParam_i32(p_card, SPC_M2CMD, M2CMD_CARD_RESET);
 }
 
@@ -303,7 +290,7 @@ uint32 AWG::reset_card() {
  * @brief Stop the card
  * @return Error code
  */
-uint32 AWG::stop_card() {
+int AWG::stop_card() {
     return spcm_dwSetParam_i32(p_card, SPC_M2CMD, M2CMD_CARD_STOP);
 }
 
@@ -323,8 +310,8 @@ void AWG::close_card() { spcm_vClose(p_card); }
  * SPCSEQ_ENDLOOPONTRIG, SPCSEQ_END)
  * @return error code
  */
-uint32 AWG::seqmem_update(int64 lStep, int64 llSegment, int64 llLoop,
-                          int64 llNext, uint64 llCondition) {
+int AWG::seqmem_update(int64 lStep, int64 llSegment, int64 llLoop, int64 llNext,
+                       uint64 llCondition) {
     uint64 llValue =
         (llCondition << 32) | (llLoop << 32) | (llNext << 16) | (llSegment);
 
@@ -332,14 +319,13 @@ uint32 AWG::seqmem_update(int64 lStep, int64 llSegment, int64 llLoop,
 }
 
 /**
- * @brief: Wirte to segment and returns status
+ * @brief: Write to segment and returns status
  * @param seg_num => sequence segment number to refer to
  * @param p_data => pointer toa source data
  * @param size => size in bytes to write
  */
-uint32 AWG::load_data(uint32 seg_num, short *p_data, uint64 size) {
-
-/// select segment to upload to
+int AWG::load_data(int seg_num, short *p_data, uint64 size) {
+    /// select segment to upload to
 #ifdef ENABLE_CUDA
     spcm_dwDefTransfer_i64(p_card, SPCM_BUF_DATA, SPCM_DIR_GPUTOCARD, 0, p_data,
                            0, size);
@@ -358,12 +344,11 @@ uint32 AWG::load_data(uint32 seg_num, short *p_data, uint64 size) {
  * @param p_data => pointer toa source data
  * @param size => size in bytes to write
  */
-uint32 AWG::load_data_async_start(uint32 seg_num, short *p_data, uint64 size) {
+int AWG::load_data_async_start(int seg_num, short *p_data, uint64 size) {
 
     int dwSegLenByte = dwFactor * lSetChannels * size;
     /// select segment to upload to
     spcm_dwSetParam_i32(p_card, SPC_SEQMODE_WRITESEGMENT, seg_num);
-
     /// Transfer of data from PC memory to on-board memory of the card
 #ifdef ENABLE_CUDA
     spcm_dwDefTransfer_i64(p_card, SPCM_BUF_DATA, SPCM_DIR_GPUTOCARD, 0, p_data,
@@ -380,7 +365,7 @@ uint32 AWG::load_data_async_start(uint32 seg_num, short *p_data, uint64 size) {
 /**
  * @brief: Waits for segment being written to finish if there is any
  */
-uint32 AWG::wait_for_data_load() {
+int AWG::wait_for_data_load() {
     return spcm_dwSetParam_i32(p_card, SPC_M2CMD, M2CMD_DATA_WAITDMA);
 }
 
@@ -388,7 +373,7 @@ uint32 AWG::wait_for_data_load() {
  * @brief initialize segment number seg_num with num_samples of samples
  * @return Error code
  */
-uint32 AWG::init_segment(uint32 seg_num, int num_samples) {
+int AWG::init_segment(int seg_num, int num_samples) {
     spcm_dwSetParam_i32(p_card, SPC_SEQMODE_WRITESEGMENT, seg_num);
     if (spcm_dwSetParam_i32(p_card, SPC_SEQMODE_SEGMENTSIZE, num_samples) !=
         ERR_OK) {
@@ -408,10 +393,10 @@ uint32 AWG::init_segment(uint32 seg_num, int num_samples) {
  * first index
  * @return status code
  */
-uint32 AWG::init_and_load_all(short *p_segment, int num_samples) {
+int AWG::init_and_load_all(short *p_segment, int num_samples) {
     int status;
     int dwSegLenByte = bps * dwFactor * num_samples * lSetChannels;
-    for (uint32 idx = 0; idx < max_seg; idx++) {
+    for (int idx = 0; idx < config.awg_num_segments; idx++) {
         status |= init_segment(idx, num_samples);
         status |= load_data(idx, p_segment, dwSegLenByte);
     }
@@ -429,8 +414,8 @@ uint32 AWG::init_and_load_all(short *p_segment, int num_samples) {
  * @param end => index write after last index to upload to
  * @return status code
  */
-uint32 AWG::init_and_load_range(short *p_segment, int num_samples, int start,
-                                int end) {
+int AWG::init_and_load_range(short *p_segment, int num_samples, int start,
+                             int end) {
     int status;
     int dwSegLenByte = bps * dwFactor * num_samples * lSetChannels;
     for (int idx = start; idx < end; idx++) {
@@ -458,7 +443,7 @@ void AWG::setup_segment_memory(int16 *pnData) {
  * @return the step at which the error was generated, i.e. the step that is
  * streaming in the sequence memory
  */
-uint32 AWG::get_current_step() {
+int AWG::get_current_step() {
     int32 current_step = INT_MAX;
     if (spcm_dwGetParam_i32(p_card, SPC_SEQMODE_STATUS, &current_step) !=
         ERR_OK) {
@@ -492,7 +477,7 @@ int AWG::allocate_transfer_buffer(int num_samples, int16 *&pnData) {
  * @param value Value to fill the transfer buffer with
  * @return status code
  */
-uint32 AWG::fill_transfer_buffer(int16 *pnData, int num_samples, int16 value) {
+int AWG::fill_transfer_buffer(int16 *pnData, int num_samples, int16 value) {
     int dwSegmentLenSample = dwFactor * num_samples;
     for (int i = 0; i < dwSegmentLenSample; i++) {
         for (int lChannel = 0; lChannel < lSetChannels; ++lChannel) {
