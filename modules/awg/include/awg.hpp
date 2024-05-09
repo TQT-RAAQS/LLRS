599215253183d8489d449cfec17a4b8278cb03fc
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-05-09 15:38:09 -0400

    Changes to the AWG modules

--- modules/awg/include/awg.hpp
+++ modules/awg/include/awg.hpp
@@ -4,7 +4,7 @@
 #include "common.hpp"
 #include "spcm_includes.h"
 
-#define MAX_SEGMENT_DIVIDE 4096
+#define AWG_MEMORY_SIZE 4294967296
 
 enum TriggerType { EMCCD };
 
@@ -13,19 +13,26 @@ class AWG {
     AWG();
     ~AWG();
 
-    uint32 start_stream();
-    uint32 reset_card();
-    uint32 stop_card();
+    int start_stream();
+    int reset_card();
+    int stop_card();
     void close_card();
 
-    uint32 set_sample_rate(int sample_rate);
-    uint32 set_external_clock_mode(int external_clock_freq);
-    uint32 set_internal_clock_mode();
-    uint32 set_trigger_settings();
-    uint32 set_dout_trigger_mode(int32 line, int32 channel);
-    uint32 set_dout_async(int32 line);
+    int seqmem_update(int64 lStep, int64 llSegment, int64 llLoop, int64 llNext,
+                      uint64 llCondition);
+    int load_data(int seg_num, short *p_data, uint64 size);
+    int load_data_async_start(int seg_num, short *p_data, uint64 size);
+    int init_segment(int seg_num, int num_samples);
+    int init_and_load_all(short *p_segment, int num_samples);
+    int init_and_load_range(short *p_segment, int num_samples, int start,
+                            int end);
+    void setup_segment_memory(int16 *pnData);
+    int wait_for_data_load();
+    int allocate_transfer_buffer(int num_samples, int16 *&pnData);
+    int fill_transfer_buffer(int16 *pnData, int num_samples, int16 value);
+    void generate_async_output_pulse(TriggerType type);
 
-    drv_handle get_card() const { return p_card; };
+    /// Getters
     int get_num_channels() const { return num_channels; };
     double get_sample_rate() const { return config.sample_rate; };
     double get_waveform_duration() const { return config.waveform_duration; };
@@ -42,31 +49,23 @@ class AWG {
     int get_null_segment_length() const { return config.null_segment_length; };
     int get_idle_segment_length() const { return config.idle_segment_length; };
     int get_wavefrom_mask() const { return config.wfm_mask; };
-
-    uint32 get_current_step();
-    uint32 get_last_seg() const { return this->max_seg - 1; };
-    uint32 get_last_step() const { return this->max_step - 1; };
-
-    uint32 seqmem_update(int64 lStep, int64 llSegment, int64 llLoop,
-                         int64 llNext, uint64 llCondition);
-    uint32 load_data(uint32 seg_num, short *p_data, uint64 size);
-    uint32 load_data_async_start(uint32 seg_num, short *p_data, uint64 size);
-    uint32 init_segment(uint32 seg_num, int num_samples);
-    uint32 init_and_load_all(short *p_segment, int num_samples);
-    uint32 init_and_load_range(short *p_segment, int num_samples, int start,
-                               int end);
-    void setup_segment_memory(int16 *pnData);
-    uint32 wait_for_data_load();
-    int allocate_transfer_buffer(int num_samples, int16 *&pnData);
-    uint32 fill_transfer_buffer(int16 *pnData, int num_samples, int16 value);
+    int get_current_step();
+    int get_last_seg() const { return config.awg_num_segments - 1; };
+    int get_last_step() const { return max_step - 1; };
     void print_awg_error();
-    uint32 read_config(std::string filename);
-    uint32 enable_channels(const std::vector<int> &channels);
-    uint32 enable_outputs(const std::vector<int> &channels,
-                          const std::vector<int> &amp);
-    uint32 generate_async_output_pulse(TriggerType type);
 
   private:
+    int set_sample_rate(int sample_rate);
+    int set_external_clock_mode(int external_clock_freq);
+    int set_internal_clock_mode();
+    int set_trigger_settings();
+    int set_dout_trigger_mode(int32 line, int32 channel);
+    int set_dout_async(int32 line);
+    int read_config(std::string filename);
+    int enable_channels(const std::vector<int> &channels);
+    int enable_outputs(const std::vector<int> &channels,
+                       const std::vector<int> &amp);
+
     struct awg_config_t {
         char *driver_path;
         int external_clock_freq;
@@ -74,8 +73,8 @@ class AWG {
         std::vector<int> amp;
         int awg_num_segments;
         double sample_rate;
-        int wfm_mask;
         double waveform_duration;
+        int wfm_mask;
         int waveforms_per_segment;
         int null_segment_length;
         int idle_segment_length;
@@ -85,18 +84,14 @@ class AWG {
         int vpp;
         int acq_timeout;
         int async_trig_amp;
-
         ~awg_config_t() { delete[] driver_path; }
-    };
-    awg_config_t config;
+    } config;
     drv_handle p_card;
     int num_channels;
-    uint32 max_step = 0;
-    uint32 max_seg = 0;
-    int bps = 0;
-    int lSetChannels = 0;
-    int dwFactor = 0;
-
-    uint32 configure();
+    int max_step;
+    int bps;
+    int lSetChannels;
+    int dwFactor;
+    int configure();
 };
 #endif
