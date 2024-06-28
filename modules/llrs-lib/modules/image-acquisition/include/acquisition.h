139fbcc7c6b0030ffd1cff9d68fa7731e95f07bb
amirdadp <86500468+amirdadp@users.noreply.github.com>
2024-06-28 14:53:42 -0400

    Made the destuctor vitural for the mock classes

--- modules/llrs-lib/modules/image-acquisition/include/acquisition.h
+++ modules/llrs-lib/modules/image-acquisition/include/acquisition.h
@@ -15,6 +15,7 @@ namespace Acquisition {
                        uint32_t roi_yoffset = 0, uint32_t vbin = 1,
                        uint32_t hbin = 1);
         virtual std::vector<uint16_t> acquire_single_image();
+        virtual ~ImageAcquisition() {};
     };
 }
 #endif
