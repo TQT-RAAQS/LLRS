af73c52bb240c29a4eedc3882c411344416738a7
Soroush Khoubyarian <soroushkhoubyarian@gmail.com>
2026-04-01 14:03:26 -0400

    typo fix

--- modules/ramsey-stabilizer/src/ramsey-stabilizer.cu
+++ modules/ramsey-stabilizer/src/ramsey-stabilizer.cu
@@ -116,7 +116,7 @@ void RamseyStabilizer::worker_function() {
                 }
                 ++images_processed;
             } else if (image_count == images_processed) { // The shot is over 
-                INFO << "Shot over.; processed all images, image count " << image_count << ".\n";
+                INFO << "Shot over; processed all images, image count " << image_count << ".\n";
                 this->smh->signal_done();
                 if (this->flag_active) {
                     this->awg_handler->stop();
